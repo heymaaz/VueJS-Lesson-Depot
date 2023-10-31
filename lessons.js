39a6ab14c2365684eb30daa0dabfd6c26491428f
Maaz Chowdhry <maazchowdhry2001@gmail.com>
2023-10-31 14:30:08 +0000

    Added lessons.js

---
+++ lessons.js
@@ -0,0 +1,96 @@
+// lessons.js
+const lessonsData = [
+    {
+        "id": 1,
+        "title": "Introduction to HTML",
+        "description": "Learn the basics of HTML and how to create a simple webpage.",
+        "price": 49.99,
+        "location": "Hatchcroft H105",
+        "image": "https://www.devopsschool.com/blog/wp-content/uploads/2022/03/html.jpg",
+        "availableInventory": 5
+    },
+
+    {
+        "id": 2,
+        "title": "CSS Styling",
+        "description": "Learn how to style your HTML pages with CSS.",
+        "price": 74.99,
+        "location": "College Building C127",
+        "image": "https://diziglobalsolution.com/wp-content/uploads/2023/04/logo-css-3-1536.png",
+        "availableInventory": 5
+    },
+    {
+        "id": 3,
+        "title": "JavaScript Fundamentals",
+        "description": "Learn the fundamentals of JavaScript programming.",
+        "price": 99.99,
+        "location": "Williams Building W122",
+        "image": "https://cdn.icon-icons.com/icons2/3398/PNG/512/javascript_logo_icon_214719.png",
+        "availableInventory": 5
+    },
+    {
+        "id": 4,
+        "title": "React for Beginners",
+        "description": "Learn how to build web applications with React.",
+        "price": 149.99,
+        "location": "College Building C212",
+        "image": "https://blog.wildix.com/wp-content/uploads/2020/06/react-logo.jpg",
+        "availableInventory": 5
+    },
+    {
+        "id": 5,
+        "title": "Node.js Basics",
+        "description": "Learn the basics of Node.js and how to build a simple server.",
+        "price": 74.99,
+        "location": "Williams Building W154",
+        "image": "https://miro.medium.com/v2/resize:fit:800/1*bc9pmTiyKR0WNPka2w3e0Q.png",
+        "availableInventory": 5
+    },
+    {
+        "id": 6,
+        "title": "MongoDB Fundamentals",
+        "description": "Learn the fundamentals of MongoDB and how to use it with Node.js.",
+        "price": 99.99,
+        "location": "Hatchcroft H116",
+        "image": "https://repvue.imgix.net/a9yxc48y3ay5dm2udzwizc2bdyph",
+        "availableInventory": 5
+    },
+    {
+        "id": 7,
+        "title": "Python Programming",
+        "description": "Learn the basics of Python programming.",
+        "price": 124.99,
+        "location": "Hatchcroft H114",
+        "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/Python.svg/1200px-Python.svg.png",
+        "availableInventory": 5
+    },
+    {
+        "id": 8,
+        "title": "Django Web Development",
+        "description": "Learn how to build web applications with Django.",
+        "price": 199.99,
+        "location": "Williams Building W104",
+        "image": "https://www.fullstackpython.com/img/logos/django.png",
+        "availableInventory": 5
+    },
+    {
+        "id": 9,
+        "title": "iOS App Development",
+        "description": "Learn how to build iOS applications with Swift.",
+        "price": 249.99,
+        "location": "College Building C135",
+        "image": "https://developer.apple.com/news/images/og/ios-17-og-twitter.jpg",
+        "availableInventory": 5
+    },
+    {
+        "id": 10,
+        "title": "Android App Development",
+        "description": "Learn how to build Android applications with Java.",
+        "price": 249.99,
+        "location": "Online",
+        "image": "https://upload.wikimedia.org/wikipedia/commons/e/ea/Android_logo_2023_%28stacked%29.svg",
+        "availableInventory": 5
+    }
+];
+
+export default lessonsData;
